4393ad4b8347a19095a708ec768e85fedacbd12f
lukebrichey <lukebrichey@gmail.com>
2024-01-19 18:08:54 -0500

    tidy: create Hero directory and update Hero function name

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-import HeroDark from '../../public/hero-dark.png'
+import HeroDark from '../../../public/hero-dark.png'
 
-export default function Component() {
+export default function Hero() {
     return (
       <section className="w-full py-12">
         <div className="container px-4 md:px-6">
