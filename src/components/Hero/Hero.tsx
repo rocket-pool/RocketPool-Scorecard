54a38702936ecec3350618d20b6a12be3d039021
lukebrichey <lukebrichey@gmail.com>
2024-01-21 13:19:20 -0500

    style: change coloring and text alignment in Hero text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -13,16 +13,16 @@ export default function Hero() {
             />
             <div className="flex flex-col justify-center space-y-4">
               <div className="space-y-2">
-                <h1 className="text-header font-poppins font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                <h1 className="text-header font-poppins font-bold tracking-tighter whitespace-nowrap sm:text-5xl xl:text-6xl/none">
                   Rocket Pool Scorecard
                 </h1>
-                <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
+                <p className="max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                   A scorecard of how the Rocket Pool community scores itself on factors including
                   risk, decentralization, security, and more.  
                 </p>
               </div>
-              <div className="bg-gray-100 p-4 rounded-lg dark:bg-gray-800 w-1/2">
-                <h2 className="text-lg font-semibold">About the Scorecard</h2>
+              <div className="bg-gray-100 p-10 rounded-lg dark:bg-slateGray w-5/6">
+                <h2 className="text-3xl font-poppins font-bold mb-4">About the Scorecard</h2>
                 <p className="text-sm text-gray-500 dark:text-gray-400">
                   Rocket Pool strives to create the most optimized community-governed
                   liquidity staking protocol. 
