50e84913c09141e79ed6787cc29c0bc72e97487d
lukebrichey <lukebrichey@gmail.com>
2024-01-27 12:23:47 -0500

    Improve table readability by increasing row padding

--- src/components/Scorecard/Table.tsx
+++ src/components/Scorecard/Table.tsx
@@ -6,21 +6,22 @@ export default function Table({ rows }: { rows: Row[] }) {
     <table className="lg:w-full mb-20 lg:border lg:border-accentGray rounded-lg">
       <thead className="dark:bg-black/07 hidden md:table-header-group">
         <tr>
-          <th className="px-6 py-3 text-left text-xs font-medium dark:text-white tracking-wider w-1/5">
+          <th className="px-6 py-5 text-left text-xs font-medium dark:text-white tracking-wider w-1/4">
             Scorecard Attribute
           </th>
-          <th className="px-6 py-3 text-left text-xs font-medium dark:text-white tracking-wider w-1/6">
+          <th className="px-6 py-5 text-left text-xs font-medium dark:text-white tracking-wider w-1/6">
             Category
           </th>
-          <th className="px-6 py-3 text-left text-xs font-medium dark:text-white tracking-wider whitespace-nowrap w-1/12">
+          <th className="px-6 py-5 text-left text-xs font-medium dark:text-white tracking-wider whitespace-nowrap w-1/12">
             Community Members at Risk
           </th>
-          <th className="px-6 py-3 text-left text-xs font-medium dark:text-white tracking-wider whitespace-nowrap w-1/5">
+          <th className="px-6 py-5 text-left text-xs font-medium dark:text-white tracking-wider whitespace-nowrap w-1/8">
             Self-Assessment
           </th>
-          <th className="px-6 py-3 text-left text-xs font-medium dark:text-white tracking-wider w-1/4">
+          <th className="px-6 py-5 text-left text-xs font-medium dark:text-white tracking-wider w-1/4">
             Comments
           </th>
+          {/* Table header for feedback link */}
           <th className="w-1/12"></th>
         </tr>
       </thead>
