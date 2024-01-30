8ca0444b11511abdd986ccd89842f5bdfc311e5b
lukebrichey <lukebrichey@gmail.com>
2024-01-30 11:04:13 -0500

    add signature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Hero from '../components/Hero/Hero'
 import Scorecard from '../components/Scorecard/Scorecard'
 import DarkModeButton from '../components/DarkModeButton'
+import Image from 'next/image'
+import AnthiasLogo from '../../public/anthias.svg'
 import './globals.css'
 
 export default function Home() {
@@ -14,6 +16,19 @@ export default function Home() {
       </div>
       <div className="mb-10">
         <DarkModeButton />
+      </div>
+      <div className="mb-10">
+        <a href="https://www.anthias.xyz" target="_blank" rel="noreferrer" className="dark:text-white/40">
+          <span>
+            Built by 
+            <Image 
+              src={AnthiasLogo} 
+              alt="Anthias Logo"
+              className="inline-block ml-2 mb-0.5 filter invert dark:filter-none"
+              unoptimized={true} 
+            /> 
+          </span>
+        </a>
       </div>
     </main>
   )
